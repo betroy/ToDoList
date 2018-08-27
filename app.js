8de07cd9b97a820caad64981f1c5df0bb6c0c14f
Troy <chenlongfeiceo@gmail.com>
2018-08-27 19:21:21 +0800

    添加page

--- app.js
+++ app.js
@@ -1,39 +1,53 @@
 //app.js
 App({
-  onLaunch: function () {
+  onLaunch(options) {
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    var logs = wx.getStorageSync("logs") || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync("logs", logs);
 
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
-    })
+    });
     // 获取用户信息
     wx.getSetting({
       success: res => {
-        if (res.authSetting['scope.userInfo']) {
+        if (res.authSetting["scope.userInfo"]) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+              this.globalData.userInfo = res.userInfo;
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                this.userInfoReadyCallback(res);
               }
             }
-          })
+          });
         }
       }
-    })
+    });
+
+    console.log("path===>" + options.path);
+  },
+  onShow(options) {
+    console.log("onShow");
+  },
+  onHide() {
+    console.log("onHide");
+  },
+  onError(error) {
+    console.log("onError");
+  },
+  onPageNotFound(options) {
+    console.log("onPageNotFound");
   },
   globalData: {
     userInfo: null
   }
-})
+});
--- pages/todo/todo.js
+++ pages/todo/todo.js
@@ -1,11 +1,21 @@
 Page({
   data: {
-      msg:"msg"
+    msg: "msg",
+    array: [1, 2, 3, 4, 5]
   },
-  onLoad: function() {},
-  viewTap:function(){
-      this.setData({
-          msg:"newMsg"
-      })
+  onLoad() {},
+  onReady() {},
+  onShow() {},
+  onHide() {},
+  onUnload() {},
+  onPullDownRefresh() {},
+  onReachBottom() {},
+  onShareAppMessage() {},
+  onPageScroll() {},
+  onTabItemTap(item) {},
+  viewTap() {
+    this.setData({
+      msg: "newMsg"
+    });
   }
 });
