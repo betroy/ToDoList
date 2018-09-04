67a41bf3480ab409d27adf7fdf14b17d20aa68a3
Troy <chenlongfeiceo@gmail.com>
2018-09-04 18:46:44 +0800

    todo list update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //app.js
 App({
   systemInfo: null,
+
   onLaunch(options) {
     const self = this;
     wx.BaaS = requirePlugin("sdkPlugin");
@@ -10,25 +11,32 @@ App({
     let clientID = "e2d25da77dc83e55a453";
     wx.BaaS.init(clientID);
 
-    // wx.BaaS.login(true).then(
-    //   res => {
-    //     console.log("success");
-    //     let MyUser = new wx.BaaS.User();
-    //     console.log(MyUser);
-    //   },
-    //   res => {
-    //     console.log("fail");
-    //   }
-    // );
+    //同步获取id
+    try {
+      let localId = wx.getStorageSync("id");
+      self.globalData.id = localId;
+      console.log("id===>" + localId);
+    } catch (e) {
+      console.log(e);
+    }
 
-    wx.checkSession({
-      success: function() {
-        console.log("check success");
-      },
-      fail: function() {
-        console.log("check fail");
-      }
-    });
+    if (!self.globalData.id) {
+      wx.BaaS.login(false).then(
+        res => {
+          console.log("login success");
+          console.log(res);
+          //保存id
+          wx.setStorage({
+            key: "id",
+            data: res.id
+          });
+        },
+        err => {
+          console.log("login fail");
+          console.log(err);
+        }
+      );
+    }
 
     wx.getSystemInfo({
       success(res) {
@@ -40,28 +48,6 @@ App({
     //   success: res => {
     //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
     //     console.log(res);
-    //   }
-    // });
-
-    // 获取用户信息
-    // wx.getSetting({
-    //   success: res => {
-    //     if (res.authSetting["scope.userInfo"]) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           console.log(res.userInfo);
-    //           // 可以将 res 发送给后台解码出 unionId
-    //           this.globalData.userInfo = res.userInfo;
-
-    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //           // 所以此处加入 callback 以防止这种情况
-    //           if (this.userInfoReadyCallback) {
-    //             this.userInfoReadyCallback(res);
-    //           }
-    //         }
-    //       });
-    //     }
     //   }
     // });
   },
@@ -78,6 +64,8 @@ App({
     console.log("onPageNotFound");
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    openid: null,
+    id: null
   }
 });
--- pages/todo/todo.js
+++ pages/todo/todo.js
@@ -2,62 +2,166 @@ const App = getApp();
 Page({
   data: {
     windowHeight: App.systemInfo.windowHeight,
-    condition: true,
-    msg: "msg",
-    array: [{ message: "foo" }, { message: "bar" }],
-    objectArray: [
-      { id: 1, unique: "unique_1" },
-      { id: 2, unique: "unique_2" },
-      { id: 3, unique: "unique_3" }
-    ],
-    items: [
-      { value: "USA", name: "美国" },
-      { value: "CHN", name: "中国", checked: "true" },
-      { value: "BRA", name: "巴西" },
-      { value: "JPN", name: "日本" },
-      { value: "JPN", name: "日本" },
-      { value: "JPN", name: "日本" },
-      { value: "JPN", name: "日本" },
-      { value: "JPN", name: "日本" },
-      { value: "ENG", name: "英国" },
-      { value: "ENG", name: "英国" },
-      { value: "ENG", name: "英国" },
-      { value: "ENG", name: "英国" },
-      { value: "ENG", name: "英国" },
-      { value: "ENG", name: "英国" },
-      { value: "ENG", name: "英国" },
-      { value: "FRA", name: "法国" }
-    ]
+    todos: [],
+    checkedTodos: []
   },
+
+  setTodos(todos) {
+    let checkedTodos = todos
+      .filter(todo => {
+        return todo.checked;
+      })
+      .map(todo => {
+        if (todo.checked) {
+          return todo._id;
+        }
+      });
+
+    this.setData({
+      todos,
+      checkedTodos
+    });
+  },
+
+  loadDone() {
+    wx.hideLoading();
+    wx.stopPullDownRefresh();
+  },
+
+  queryTodos() {
+    //实例化TableObject对象
+    let todoTableObject = new wx.BaaS.TableObject("todo");
+    //实例化Query对象
+    let query = new wx.BaaS.Query();
+    query.compare("created_by", "=", App.globalData.id);
+    todoTableObject
+      .setQuery(query)
+      .limit(15)
+      .offset(this.data.todos.length)
+      .find()
+      .then(
+        res => {
+          console.log("query todo success");
+          console.log(res);
+          let todos = res.data.objects;
+          let currentTodos = this.data.todos;
+          if (todos && todos.length > 0) {
+            this.setTodos([...currentTodos, ...todos]);
+          }
+          this.loadDone();
+        },
+        err => {
+          console.log("query todo err");
+          console.log(err);
+          this.loadDone();
+        }
+      );
+  },
+
+  addTodo(title) {
+    //实例化TableObject对象
+    let todoTableObject = new wx.BaaS.TableObject("todo");
+    //创建一条空记录
+    let todoRecord = todoTableObject.create();
+    todoRecord.set("todo_title", title);
+    todoRecord.set("checked", false);
+    todoRecord.save().then(
+      res => {
+        console.log("add todo success");
+        console.log(res);
+        this.setTodos([...this.data.todos, res.data]);
+      },
+      err => {
+        console.log("add todo err");
+        console.log(err);
+      }
+    );
+  },
+
+  deleteTodo() {},
+
+  updateTodo(currentCheckedTodos) {
+    //查找当前已选中的todo
+    // let checkedTodos = this.data.todos.filter(todo => todo.checked);
+    let checkedTodos = this.data.checkedTodos;
+    //对比，找出已修改状态的todo
+    //差集
+    var differenceTodo = checkedTodos
+      .filter(todo => {
+        return currentCheckedTodos.indexOf(todo) == -1;
+      })
+      .concat(
+        currentCheckedTodos.filter(todo => {
+          return checkedTodos.indexOf(todo) == -1;
+        })
+      );
+    console.log(checkedTodos);
+    console.log(currentCheckedTodos);
+    console.log(differenceTodo);
+    this.setData({
+      checkedTodos: currentCheckedTodos
+    });
+    let checkedTodoId = differenceTodo[0];
+
+    let todos = this.data.todos;
+    let currentCheckedTodo = null;
+
+    for (var i in todos) {
+      if (todos[i]._id === checkedTodoId) {
+        currentCheckedTodo = todos[i];
+        break;
+      }
+    }
+    console.log(currentCheckedTodo);
+    //实例化TableObject对象
+    let todoTableObject = new wx.BaaS.TableObject("todo");
+    let todoRecord = todoTableObject.getWithoutData(currentCheckedTodo._id);
+    todoRecord.set("checked", !currentCheckedTodo.checked);
+    todoRecord.update().then(
+      res => {
+        console.log("update todo success");
+      },
+      err => {
+        console.log("update todo err");
+      }
+    );
+  },
+
   onLoad() {
-    // let todoTableObject = new wx.BaaS.TableObject(49981);
-    // let myRecord = todoTableObject.create();
-    // myRecord.set("task_title", "task_title");
-    // myRecord.save();
+    wx.showLoading({
+      title: "加载中"
+    });
   },
-  onReady() {},
+  onReady() {
+    this.queryTodos();
+  },
   onShow() {},
   onHide() {},
   onUnload() {},
   onPullDownRefresh() {
     console.log("onPullDownRefresh");
-    wx.stopPullDownRefresh();
+    this.queryTodos();
   },
   onReachBottom() {},
   onShareAppMessage() {},
   onPageScroll() {},
   onTabItemTap(item) {},
   bindconfirm(event) {
-    var value = event.detail.value;
+    let title = event.detail.value;
     console.log(event.detail.value);
     this.setData({
       //清空input value
       inputValue: ""
     });
+    if (title) {
+      this.addTodo(title);
+    }
   },
   scrolltolower(event) {
+    //加载更多
     console.log("scrolltolower");
     console.log(event);
+    this.queryTodos();
   },
   //item点击事件 跳转至任务详情
   itemBindTap(event) {
@@ -65,9 +169,11 @@ Page({
     wx.navigateTo({ url: "../tododetail/tododetail" });
   },
   checkboxChange(event) {
-    console.log(event);
+    // console.log(event);
+    // let currentCheckedTodos = event.detail.value;
+    // this.updateTodo(currentCheckedTodos);
   },
   catchtap(event) {
-    // console.log(event);
+    console.log(event);
   }
 });
